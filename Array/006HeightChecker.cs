19e53fde0d29f26dc9d5f94b07fc2d47ef2a2227
JackY <thunder_blow@outlook.com>
2023-06-11 22:40:23 +0700

    Solve array test

---
+++ Array/006HeightChecker.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public class 0006HeightChecker
+    {
+        
+    }
+}
