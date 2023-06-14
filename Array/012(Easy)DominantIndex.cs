9f19f380e71199c817943156f82da1de4fca5359
JackY <thunder_blow@outlook.com>
2023-06-14 22:26:18 +0700

    finish test array 012

---
+++ Array/012(Easy)DominantIndex.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/*You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,1,0]
+Output: 1
+Explanation: 6 is the largest integer.
+For every other number in the array x, 6 is at least twice as big as x.
+The index of value 6 is 1, so we return 1.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: -1
+Explanation: 4 is less than twice the value of 3, so we return -1.*/
+namespace Array
+{
+    public class DominantIndex
+    {
+        public int DominantIndex(int[] nums)
+        {
+            int maxValue = 0;
+            int Index = 0;
+            foreach (int x in nums) { maxValue = Math.Max(x, maxValue); }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i]==maxValue){
+                    Index = i;
+                }
+
+                if (nums[i] * 2 > maxValue&&nums[i] != maxValue)
+                {
+                    return -1;
+                }
+            }
+
+            return Index;
+        }
+    }
+}
