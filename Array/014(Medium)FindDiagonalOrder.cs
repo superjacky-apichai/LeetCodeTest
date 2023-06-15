a9b6917fa95b03207c83c33175f6477bc12d9733
JackY <thunder_blow@outlook.com>
2023-06-16 00:54:51 +0700

    stuck 014

--- Array/014(Medium)FindDiagonalOrder.cs
+++ Array/014(Medium)FindDiagonalOrder.cs
@@ -9,7 +9,88 @@ namespace Array
     {
         public int[] FindDiagonalOrder(int[][] mat)
         {
+            IList<int> res = new List<int>();
+            if (mat.Length < 1)
+            {
+                for (int i = 0; i < mat.Rank; i++)
+                {
+                    for (int j = 0; j < mat[i].Length; j++)
+                    {
+                        res.Add(mat[i][j]);
+                    }
+                }
 
+                return res.ToArray();
+            }
+            else if (mat[0].Length < 1)
+            {
+                for (int i = 0; i < mat.Rank; i++)
+                {
+                    
+                        res.Add(mat[i][0]);
+                    
+                }
+            }
+
+            bool isUpOrDown = true;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                int count = i;
+                for (int j = 0; j < mat.Length; j++)
+                {
+                    int coutj = j;
+                    if (isUpOrDown)
+                    {
+                        res.Add(mat[count][j]);
+                    }
+                    else if (!isUpOrDown)
+                    {
+                        res.Add(mat[j][count]);
+                    }
+
+
+                    if (j == i)
+                    {
+                        isUpOrDown = isUpOrDown ? false : true;
+                        break;
+                    }
+                    count--;
+
+                }
+            }
+
+
+            for (int j = 1; j < mat.Length; j++)
+            {
+                int len = mat.Length - 1;
+                int countj = j;
+                while (true)
+                {
+                    if (isUpOrDown)
+                    {
+                        res.Add(mat[len][countj]);
+                    }
+                    else if (!isUpOrDown)
+                    {
+                        res.Add(mat[countj][len]);
+                    }
+                    len--;
+                    countj++;
+
+                    if (j > len)
+                    {
+                        isUpOrDown = isUpOrDown ? false : true;
+                        break;
+                    }
+
+
+                }
+
+            }
+
+            return res.ToArray();
         }
+
+
     }
 }
