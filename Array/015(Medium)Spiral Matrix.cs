bdb7c4440761baa01203bed2385bcf874caa22f3
JackY <thunder_blow@outlook.com>
2023-06-18 20:46:44 +0700

    Finish array 015

--- Array/015(Medium)Spiral Matrix.cs
+++ Array/015(Medium)Spiral Matrix.cs
@@ -8,8 +8,8 @@ namespace Array
     public class SpiralMatrix
     {
         public IList<int> SpiralOrder(int[][] matrix)
-        {        
-        
+        {
+
             int[] res = new int[matrix.Length * matrix[0].Length];
             int count = 0;
             int i = 0;
@@ -21,12 +21,11 @@ namespace Array
             int minRow = 1;
             int minColum = 0;
 
-
-            while (count < (matrix.Length * matrix[i].Length))
-            {               
+            while (count < res.Length)
+            {
                 res[count] = matrix[i][j];
-                
-                    count++;
+
+                count++;
 
                 if (switchRowOrColum)
                 {
@@ -54,6 +53,7 @@ namespace Array
                     if (switchUpAndLeftOrDownAndRigth)
                     {
                         j--;
+
                         if (j == minColum)
                         {
                             switchRowOrColum = true;
@@ -64,7 +64,13 @@ namespace Array
                     {
                         j++;
 
-                        if (j == maxColum)
+                        if (j > maxColum)
+                        {
+                            j--;
+                            i++;
+                            switchRowOrColum = true;
+                        }
+                        else if (j == maxColum)
                         {
                             switchRowOrColum = true;
                         }
@@ -83,11 +89,44 @@ namespace Array
                     switchUpAndLeftOrDownAndRigth = false;
                 }
 
-                  
+
             }
 
 
             return res.ToList();
         }
+
+
+
+
+        /* Great Solution */
+
+        public IList<int> SpiralOrder(int[][] matrix)  {
+        int width=matrix[0].Length;
+        int height=matrix.Length;
+        int length=width*height;
+        List<int> ans = new List<int>(length);
+        int x=0,y=0,dx=1,dy=1;
+        while(ans.Count<length)
+        {
+            ans.Add(matrix[y][x]);
+            if(y==dy-1 && x<width-dx)
+                x++;
+            else if(x==width-dx && y<height-dy)
+                y++;
+            else if(x>dx-1) 
+                x--;
+            else if(y>dy)
+            {
+                y--;
+                if(y==dy && x==dx-1)
+                {
+                    dx++;
+                    dy++;
+                }
+            }
+        }
+        return ans;
+    }
     }
 }
