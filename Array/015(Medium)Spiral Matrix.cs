2a82702a3629b5c8161720726f03b3f2e86fd5b1
JackY <thunder_blow@outlook.com>
2023-06-18 16:27:42 +0700

    stuck Array 015

--- Array/015(Medium)Spiral Matrix.cs
+++ Array/015(Medium)Spiral Matrix.cs
@@ -7,6 +7,85 @@ namespace Array
 {
     public class SpiralMatrix
     {
-        
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            int[] res = new int[matrix.Length * matrix[0].Length];
+            int count = 0;
+            int i = 0;
+            int j = 0;
+            bool switchRowOrColum = false;
+            bool switchUpAndLeftOrDownAndRigth = false;
+            int maxRow = matrix.Length - 1;
+            int maxColum = matrix.Length - 1;
+            int minRow = 1;
+            int minColum = 0;
+
+
+            while (count < matrix.Length * matrix[i].Length)
+            {
+                res[count] = matrix[i][j];
+                count++;
+
+                if (switchRowOrColum)
+                {
+                    if (switchUpAndLeftOrDownAndRigth)
+                    {
+                        i--;
+                        if (i == minRow)
+                        {
+                            switchRowOrColum = false;
+                        }
+                    }
+                    else if (!switchUpAndLeftOrDownAndRigth)
+                    {
+                        i++;
+                        if (i == maxRow)
+                        {
+                            switchRowOrColum = false;
+                        }
+                    }
+
+                }
+                else if (!switchRowOrColum)
+                {
+
+                    if (switchUpAndLeftOrDownAndRigth)
+                    {
+                        j--;
+                        if (j == minColum)
+                        {
+                            switchRowOrColum = true;
+
+                        }
+                    }
+                    else if (!switchUpAndLeftOrDownAndRigth)
+                    {
+                        j++;
+
+                        if (j == maxColum)
+                        {
+                            switchRowOrColum = true;
+                        }
+                    }
+                }
+                if (i == maxRow && j == maxColum)
+                {
+                    maxRow--;
+                    maxColum--;
+                    switchUpAndLeftOrDownAndRigth = true;
+                }
+                else if (i == minRow && j == minColum)
+                {
+                    minRow++;
+                    minColum++;
+                    switchUpAndLeftOrDownAndRigth = false;
+                }
+
+
+            }
+
+
+            return res.ToList();
+        }
     }
 }
