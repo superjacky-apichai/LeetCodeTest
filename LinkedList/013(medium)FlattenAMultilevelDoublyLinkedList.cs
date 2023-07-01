1d506703299ab46fa1cda7f34804d3a02ce11a84
JackY <thunder_blow@outlook.com>
2023-07-01 11:22:02 +0700

    finish linked list 013

--- LinkedList/013(medium)FlattenAMultilevelDoublyLinkedList.cs
+++ LinkedList/013(medium)FlattenAMultilevelDoublyLinkedList.cs
@@ -2,7 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+/*You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
 
+Given the head of the first level of the list, flatten the list so that all the nodes appear in a single-level, doubly linked list. Let curr be a node with a child list. The nodes in the child list should appear after curr and before curr.next in the flattened list.
+
+Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+Explanation: The multilevel linked list in the input is shown.
+After flattening the multilevel linked list it becomes:
+
+Example 2:
+
+
+Input: head = [1,2,null,3]
+Output: [1,3,2]
+Explanation: The multilevel linked list in the input is shown.
+After flattening the multilevel linked list it becomes:
+
+Example 3:
+
+Input: head = []
+Output: []
+Explanation: There could be empty list in the input.
+ 
+
+Constraints:
+
+The number of Nodes will not exceed 1000.
+1 <= Node.val <= 105
+ 
+
+How the multilevel linked list is represented in test cases:
+
+We use the multilevel linked list from Example 1 above:
+
+ 1---2---3---4---5---6--NULL
+         |
+         7---8---9---10--NULL
+             |
+             11--12--NULL
+The serialization of each level is as follows:
+
+[1,2,3,4,5,6,null]
+[7,8,9,10,null]
+[11,12,null]
+To serialize all levels together, we will add nulls in each level to signify no node connects to the upper node of the previous level. The serialization becomes:
+
+[1,    2,    3, 4, 5, 6, null]
+             |
+[null, null, 7,    8, 9, 10, null]
+                   |
+[            null, 11, 12, null]
+Merging the serialization of each level and removing trailing nulls we obtain:
+
+[1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]*/
 namespace LinkedList
 {
 
@@ -26,10 +86,83 @@ namespace LinkedList
                 return null;
             }
 
-            ListNode temp = head;
-            ListNode newNode = new ListNode(0);
-            ListNode newTemp = newNode;
 
+            Node temp = head;
+            Node newNode = new Node(0);
+            Node newTemp = newNode;
+            Node unFill = new Node(0);
+
+            while (temp != null)
+            {
+
+                newTemp.next = temp;
+                newTemp.next.prev = newTemp;
+                newTemp = newTemp.next;
+
+                if (temp.child != null)
+                {
+                    if (temp.next != null)
+                    {
+                        if (unFill.next == null)
+                        {
+                            unFill.next = temp.next;
+                            unFill.next.prev = unFill;
+                            temp.next = null;
+
+                        }
+                        else if (unFill.next != null)
+                        {
+                            Node tempFill = unFill.next;
+                            unFill.next = temp.next;
+                            unFill.next.prev = unFill;
+                            Node Findlast = FindLastNode(unFill);
+                            Findlast.next = tempFill;
+                            Findlast.next.prev = Findlast;
+                            temp.next = null;
+                            Findlast = null;
+                            tempFill = null;
+
+
+                        }
+
+                    }
+                    temp = temp.child;
+                    newTemp.child = null;
+
+
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+
+            }
+            temp = null;
+
+            if (unFill.next != null)
+            {
+                unFill = unFill.next;
+                newTemp.next = unFill;
+                newTemp.next.prev = newTemp;
+            }
+
+            newNode = newNode.next;
+            newNode.prev = null;
+            return newNode;
+        }
+
+        public Node FindLastNode(Node node)
+        {
+
+            Node temp = node;
+
+            while (temp.next != null)
+            {
+
+                temp = temp.next;
+            }
+            node = null;
+            return temp;
 
         }
 
