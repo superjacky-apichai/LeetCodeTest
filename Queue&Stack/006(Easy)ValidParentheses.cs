05a5e51190512a3a78b1d5d599a16f0601e5ec0a
JackY <thunder_blow@outlook.com>
2023-07-17 20:17:31 +0700

    create 006

---
+++ Queue&Stack/006(Easy)ValidParentheses.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.*/
+namespace QueueStack
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+
+        }
+    }
+}
