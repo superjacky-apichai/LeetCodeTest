7454978633a0532c3acfa2fd175e0cae1d6510a7
JackY <thunder_blow@outlook.com>
2023-07-20 09:55:02 +0700

    finish stack 009

--- Queue&Stack/009(Medium)NumberofIslands.cs
+++ Queue&Stack/009(Medium)NumberofIslands.cs
@@ -33,14 +33,92 @@ Constraints:
 m == grid.length
 n == grid[i].length
 1 <= m, n <= 300
-grid[i][j] is '0' or '1'.*/
+grid[i][j] is '0' or '1'.
+
+
+[["1","1","1"],["0","1","0"],["1","1","1"]]
+
+
+[["1","1","1"],
+ ["0","1","0"],
+ ["1","1","1"]]
+ 
+ [["1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","0","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","0","0","0","0","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1"]]*/
 namespace QueueStack
 {
     public class NumberofIslands
     {
         public int NumIslands(char[][] grid)
         {
-               
+
+            Stack<int[]> stack = new Stack<int[]>();
+            int count = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        stack.Push(new int[]{i,j});
+                        grid[i][j] = '0';
+
+                        while (stack.Count > 0)
+                        {
+                            int i1 = stack.Peek()[0];
+                            int j1 = stack.Peek()[1];
+                            stack.Pop();
+                            if (i1 + 1 < grid.Length)
+                            {
+                                if (grid[i1 + 1][j1] == '1')
+                                {
+                                    stack.Push(new int[]{i1+1,j1});;
+                                    grid[i1+1][j1] = '0';
+                                }
+                            }
+
+                            if (j1 + 1 < grid[0].Length)
+                            {
+                                if (grid[i1][j1 + 1] == '1')
+                                {
+                                   stack.Push(new int[]{i1,j1+1});;
+                                    grid[i1][j1 + 1] = '0';
+                                }
+                            }
+
+                            if (i1 - 1 >= 0)
+                            {
+                                if (grid[i1 - 1][j1] == '1')
+                                {
+                                    stack.Push(new int[]{i1-1,j1});;
+                                    grid[i1 - 1][j1] = '0';
+                                }
+
+                            }
+                            if (j1 - 1 >= 0)
+                            {
+                                if (grid[i1][j1 - 1] == '1')
+                                {
+                                     stack.Push(new int[]{i1,j1-1});;
+                                    grid[i1 ][j1-1] = '0';
+                                }
+
+                            }
+
+                        }
+
+
+
+                        count++;
+                    }
+
+
+
+                }
+
+            }
+            return count;
+
         }
     }
 }
